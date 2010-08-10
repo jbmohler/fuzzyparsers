7937af1a38f78f5b496df1af4fc3f3dfea20e4f7
Joel B. Mohler <joel@kiwistrawberry.us>
2010-08-10 05:34:59 -0400

    add GPL license documentation

--- fuzzyparsers/fuzzyparsers.py
+++ fuzzyparsers/fuzzyparsers.py
@@ -1,3 +1,9 @@
+##############################################################################
+#       Copyright (C) 2010, Joel B. Mohler <joel@kiwistrawberry.us>
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#                  http://www.gnu.org/licenses/
+##############################################################################
 # -*- coding: utf-8 -*-
 
 import datetime
