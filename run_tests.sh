b8c1fdbe07fb5045b217f4dff805b6771a906574
Joel B. Mohler <joel@kiwistrawberry.us>
2012-04-28 13:56:14 -0400

    remove doc-tests tidbits which are better configured standard ways

--- run_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo python setup.py install
-nosetests --with-doctest --with-coverage --cover-html --cover-package=fuzzyparsers --cover-tests --cover-erase --verbose
-python -m doctest README.txt
-
-xdg-open cover/index.html
