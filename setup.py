77240da8c964096607bf15327792d8d65bd37c0a
Joel B. Mohler <joel@kiwistrawberry.us>
2022-05-19 13:44:39 -0400

    declare as markdown correctly

--- setup.py
+++ setup.py
@@ -13,12 +13,13 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='fuzzyparsers',
-    version='0.9.5',  # also update in fuzzyparsers/__init__.py
+    version='0.9.5.1',  # also update in fuzzyparsers/__init__.py
     description='A collection of free-form input parsers (with special focus on dates)',
     license='MIT',
     author='Joel B. Mohler',
     author_email='joel@kiwistrawberry.us',
     long_description=read('README.md'),
+    long_description_content_type='text/markdown',
     url='https://github.com/jbmohler/fuzzyparsers',
     packages=['fuzzyparsers'],
     classifiers = [
